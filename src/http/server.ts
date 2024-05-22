71ef7eea399dcd416cd12f92b74e620732558f13
Carlos <silva.carloseduardo.ds@gmail.com>
2024-05-22 20:08:55 -0300

    feat: create proper structure for routes and add createPoll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,11 @@
 import fastify from "fastify"
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod"
+import { createPoll } from "./routes/create-poll";
 
 const app = fastify()
 
-app.get("/hello", () => {
-  return "Hello World"
-})
+app.register(createPoll)
 
 app.listen({ port: 3333}).then(() => {
   console.log("HTTP server running!")
