a2c3fb521fef6361fc8144c0e6699d3777c4a9a2
Carlos <silva.carloseduardo.ds@gmail.com>
2024-05-25 13:17:36 -0300

    feat: update get-poll endpoint to include vote counts from Redis

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
-import { z } from "zod";
+import { object, z } from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
   app.get("/polls/:pollId", async (request, replay) => {
@@ -28,6 +29,32 @@ export async function getPoll(app: FastifyInstance) {
       return replay.status(400).send({ message: "Poll not found" })
     }
 
-    return replay.send({ poll });
+    const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+    
+    const votes = result.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        const score = result[index + 1]
+
+        Object.assign(obj, { [line]: Number(score) })
+      }
+
+      return obj
+    }, {} as Record<string, number>)
+
+    console.log(votes)
+
+    return replay.send({ 
+      poll: {
+        id: pollId,
+        title: poll.title,
+        options: poll.options.map(option => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: (option.id in votes) ? votes[option.id] : 0
+          }
+        })
+      }
+    });
   });
 }
