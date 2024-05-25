87549d0e38f5844d83cf5e99c59c9976170bf94d
Carlos <silva.carloseduardo.ds@gmail.com>
2024-05-25 17:48:39 -0300

    chore: add and setup Fastify's websocket packet

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,6 @@
 import fastify from "fastify"
 import cookie from "@fastify/cookie"
-import { PrismaClient } from "@prisma/client";
-import { z } from "zod"
+import websocket from "@fastify/websocket";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
@@ -12,6 +11,8 @@ app.register(cookie, {
   secret: "polls-app",
   hook: 'onRequest',
 })
+
+app.register(websocket)
 
 app.register(createPoll)
 app.register(getPoll)
